0ce4955d5257c6ca7c72b84787b39f09f2b47c59
Cacivy <609448234@qq.com>
2016-07-01 18:48:38 +0800

    change

--- src/utils/coerceBoolean.js
+++ src/utils/coerceBoolean.js
@@ -1,9 +1,10 @@
 // Attempt to convert a string value to a Boolean. Otherwise, return the value
 // without modification so Vue can throw a warning.
-"use strict";
 
-module["export"] = function (val) {
-  return typeof val !== "string" ? val : val === "true" ? true : val === "false" ? false : val === "null" ? false : val === "undefined" ? false : val;
-};
-
-
+module.exports = function(val){
+  return typeof val !== "string" ? val :
+val === "true" ? true :
+val === "false" ? false :
+val === "null" ? false :
+val === "undefined" ? false : val
+}
